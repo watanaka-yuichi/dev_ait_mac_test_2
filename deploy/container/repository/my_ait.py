c438721f553f49c0f0210db09d54f11fdfacd324
System Administrator <root@AIRCnoMacBook-Pro.local>
2024-06-18 17:00:08 +0900

    test

---
+++ deploy/container/repository/my_ait.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # AIT Development notebook
+
+# ## notebook of structure
+
+# | #  | Name                                               | cells | for_dev | edit               | description                                                                |
+# |----|----------------------------------------------------|-------|---------|--------------------|----------------------------------------------------------------------------|
+# | 1  | [Environment detection](##1-Environment-detection) | 1     | No      | uneditable         | detect whether the notebook are invoked for packaging or in production     |
+# | 2  | [Preparing AIT SDK](##2-Preparing-AIT-SDK)         | 1     | Yes     | uneditable         | download and install AIT SDK                                               |
+# | 3  | [Dependency Management](##3-Dependency-Management) | 3     | Yes     | required(cell #2)  | generate requirements.txt for Docker container                             |
+# | 4  | [Importing Libraries](##4-Importing-Libraries)     | 2     | Yes     | required(cell #1)  | import required libraries                                                  |
+# | 5  | [Manifest Generation](##5-Manifest-Generation)     | 1     | Yes     | required           | generate AIT Manifest                                                      |
+# | 6  | [Prepare for the Input](##6-Prepare-for-the-Input) | 1     | Yes     | required           | generate AIT Input JSON (inventory mapper)                                 |
+# | 7  | [Initialization](##7-Initialization)               | 1     | No      | uneditable         | initialization for AIT execution                                           |
+# | 8  | [Function definitions](##8-Function-definitions)   | N     | No      | required           | define functions invoked from Main area.<br> also define output functions. |
+# | 9  | [Main Algorithms](##9-Main-Algorithms)             | 1     | No      | required           | area for main algorithms of an AIT                                         |
+# | 10 | [Entry point](##10-Entry-point)                    | 1     | No      | uneditable         | an entry point where Qunomon invoke this AIT from here                     |
+# | 11 | [License](##11-License)                            | 1     | Yes     | required           | generate license information                                               |
+# | 12 | [Deployment](##12-Deployment)                      | 1     | Yes     | uneditable         | convert this notebook to the python file for packaging purpose             |
+
+# ## notebook template revision history
+
+# 1.0.1 2020/10/21
+# 
+# * add revision history
+# * separate `create requirements and pip install` editable and noeditable
+# * separate `import` editable and noeditable
+# 
+# 1.0.0 2020/10/12
+# 
+# * new cerarion
+
+# ## body
+
+# ### #1 Environment detection
+
+# [uneditable]
+
+# In[ ]:
+
+
+# Determine whether to start AIT or jupyter by startup argument
+import sys
+is_ait_launch = (len(sys.argv) == 2)
+
+
+# ### #2 Preparing AIT SDK
+
+# [uneditable]
+
+# In[2]:
+
+
+if not is_ait_launch:
+    # get ait-sdk file name
+    from pathlib import Path
+    from glob import glob
+    import re
+    import os
+
+    current_dir = get_ipython().run_line_magic('pwd', '')
+
+    ait_sdk_path = "./ait_sdk-*-py3-none-any.whl"
+    ait_sdk_list = glob(ait_sdk_path)
+    ait_sdk_name = os.path.basename(ait_sdk_list[-1])
+
+    # install ait-sdk
+    get_ipython().system('pip install -q --upgrade pip')
+    get_ipython().system('pip install -q --no-deps --force-reinstall ./$ait_sdk_name')
+
+
+# ### #3 Dependency Management
+
+# #### #3-1 [uneditable]
+
+# In[3]:
+
+
+if not is_ait_launch:
+    from ait_sdk.common.files.ait_requirements_generator import AITRequirementsGenerator
+    requirements_generator = AITRequirementsGenerator()
+
+
+# #### #3-2 [required]
+
+# In[4]:
+
+
+if not is_ait_launch:
+## sample ##
+     requirements_generator.add_package('pandas')
+     requirements_generator.add_package('seaborn')
+
+
+# #### #3-3 [uneditable]
+
+# In[5]:
+
+
+if not is_ait_launch:
+    requirements_generator.add_package(f'./{ait_sdk_name}')
+    requirements_path = requirements_generator.create_requirements(current_dir)
+
+    get_ipython().system('pip install -q -r $requirements_path ')
+
+
+# ### #4 Importing Libraries
+
+# #### #4-1 [required]
+
+# In[6]:
+
+
+# import if you need modules cell
+
+## sample ##
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+# #### #4-2 [uneditable]
+
+# In[7]:
+
+
+# must use modules
+from os import path
+import shutil  # do not remove
+from ait_sdk.common.files.ait_input import AITInput  # do not remove
+from ait_sdk.common.files.ait_output import AITOutput  # do not remove
+from ait_sdk.common.files.ait_manifest import AITManifest  # do not remove
+from ait_sdk.develop.ait_path_helper import AITPathHelper  # do not remove
+from ait_sdk.utils.logging import get_logger, log, get_log_path  # do not remove
+from ait_sdk.develop.annotation import measures, resources, downloads, ait_main  # do not remove
+# must use modules
+
+
+# ### #5 Manifest Generation
+
+# [required]
+
+# In[8]:
+
+
+if not is_ait_launch:
+## sample ##
+    from ait_sdk.common.files.ait_manifest_generator import AITManifestGenerator
+    manifest_genenerator = AITManifestGenerator(current_dir)
+    manifest_genenerator.set_ait_name('dev_ait_mac_test_1')
+    manifest_genenerator.set_ait_description('AIT template (docker image regist to remote)')
+    manifest_genenerator.set_ait_source_repository('https://github.com/aistairc/ait-template')
+    manifest_genenerator.set_ait_version('0.1')
+    manifest_genenerator.add_ait_keywords('Template')
+    manifest_genenerator.set_ait_quality('https://ait-hub.pj.aist.go.jp/ait-hub/api/0.0.1/qualityDimensions/機械学習品質マネジメントガイドライン第三版/A-2データ設計の十分性')
+    inventory_requirement_iris_data = manifest_genenerator.format_ait_inventory_requirement(format_=['csv'])
+    manifest_genenerator.add_ait_inventories(name='iris_data', 
+                                              type_='dataset', 
+                                              description='アヤメの分類データです', 
+                                              requirement=inventory_requirement_iris_data)
+    manifest_genenerator.add_ait_parameters(name='mean_column_name', 
+                                             type_='str', 
+                                             description='sepal.width\nsepal.length\npetal.width\npetal.length', 
+                                             default_val='sepal.width')
+    manifest_genenerator.add_ait_measures(name='mean', 
+                                           type_='float', 
+                                           description='mean of select column', 
+                                           structure='single',
+                                           min='0')
+    manifest_genenerator.add_ait_resources(name='pairplot', 
+                                            type_='picture', 
+                                            description='pairplot')
+    manifest_genenerator.add_ait_downloads(name='Log', 
+                                            description='AIT実行ログ')
+    manifest_path = manifest_genenerator.write()
+
+
+# ### #6 Prepare for the Input
+
+# [required]
+
+# In[9]:
+
+
+if not is_ait_launch:
+## sample ##
+    from ait_sdk.common.files.ait_input_generator import AITInputGenerator
+    input_generator = AITInputGenerator(manifest_path)
+    input_generator.add_ait_inventories(name='iris_data',
+                                         value='iris_data/tableconvert_csv_4nryby.csv')
+    input_generator.set_ait_params(name='mean_column_name',
+                                    value='petal.width')
+    input_generator.write()
+
+
+# ### #7 Initialization
+
+# [uneditable]
+
+# In[10]:
+
+
+logger = get_logger()
+
+ait_manifest = AITManifest()
+ait_input = AITInput(ait_manifest)
+ait_output = AITOutput(ait_manifest)
+
+if is_ait_launch:
+    # launch from AIT
+    current_dir = path.dirname(path.abspath(__file__))
+    path_helper = AITPathHelper(argv=sys.argv, ait_input=ait_input, ait_manifest=ait_manifest, entry_point_dir=current_dir)
+else:
+    # launch from jupyter notebook
+    # ait.input.json make in input_dir
+    input_dir = '/usr/local/qai/mnt/ip/job_args/1/1'
+    current_dir = get_ipython().run_line_magic('pwd', '')
+    path_helper = AITPathHelper(argv=['', input_dir], ait_input=ait_input, ait_manifest=ait_manifest, entry_point_dir=current_dir)
+
+ait_input.read_json(path_helper.get_input_file_path())
+ait_manifest.read_json(path_helper.get_manifest_file_path())
+
+### do not edit cell
+
+
+# ### #8 Function definitions
+
+# [required]
+
+# In[11]:
+
+
+## sample ##
+@log(logger)
+@measures(ait_output, 'mean')
+def calc_mean(iris_data, col_name):
+    return iris_data[col_name].mean()
+
+
+# In[12]:
+
+
+## sample ##
+@log(logger)
+@resources(ait_output, path_helper, 'pairplot', 'pairplot.png')
+def save_pair_plot(iris_data, file_path: str=None) -> str:    
+    sn.pairplot(iris_data, hue='variety')
+    plt.savefig(file_path)
+
+
+# In[13]:
+
+
+## sample ##
+@log(logger)
+@downloads(ait_output, path_helper, 'Log', 'ait.log')
+def move_log(file_path: str=None) -> str:
+    shutil.move(get_log_path(), file_path)
+
+
+# ### #9 Main Algorithms
+
+# [required]
+
+# In[14]:
+
+
+@log(logger)
+@ait_main(ait_output, path_helper)
+def main() -> None:
+## sample ##
+    # インベントリを読み込み
+    iris_data = pd.read_csv(ait_input.get_inventory_path('iris_data'))
+    
+    calc_mean(iris_data, ait_input.get_method_param_value('mean_column_name'))
+    save_pair_plot(iris_data)
+    move_log()
+
+
+# ### #10 Entry point
+
+# [uneditable]
+
+# In[15]:
+
+
+if __name__ == '__main__':
+    main()
+
+
+# ### #11 License
+
+# [required]
+
+# In[16]:
+
+
+## sample ##
+ait_owner='AIST'
+ait_creation_year='2020'
+
+
+# ### #12 Deployment
+
+# [uneditable] 
+
+# In[17]:
+
+
+if not is_ait_launch:
+    from ait_sdk.deploy import prepare_deploy
+    from ait_sdk.license.license_generator import LicenseGenerator
+    
+    current_dir = get_ipython().run_line_magic('pwd', '')
+    prepare_deploy(ait_sdk_name, current_dir, requirements_path)
+    
+    # output License.txt
+    license_generator = LicenseGenerator()
+    license_generator.write('../top_dir/LICENSE.txt', ait_creation_year, ait_owner)
+
+
+# In[ ]:
+
+
+
+
